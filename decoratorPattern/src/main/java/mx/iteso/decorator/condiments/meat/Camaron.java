957f31717f96473c835e5085a4216cd450a5a3c7
lilyalvarezg <lilylimon@hotmail.com>
2015-10-01 08:48:47 -0500

    decoratorPattern
    Decorator

---
+++ decoratorPattern/src/main/java/mx/iteso/decorator/condiments/meat/Camaron.java
@@ -0,0 +1,15 @@
+package mx.iteso.decorator.condiments.meat;
+
+import mx.iteso.decorator.Taco;
+
+/**
+ * Created by Lily on 01/10/2015.
+ */
+public class Camaron {
+    Taco taco;
+
+    public Camaron(Taco taco) {
+        this.taco = taco;
+    }
+
+}
---
+++ decoratorPattern/src/main/java/mx/iteso/decorator/condiments/meat/Pescado.java
@@ -0,0 +1,7 @@
+package mx.iteso.decorator.condiments.meat;
+
+/**
+ * Created by Lily on 01/10/2015.
+ */
+public class Pescado {
+}
